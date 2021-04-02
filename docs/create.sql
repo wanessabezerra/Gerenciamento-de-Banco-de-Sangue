e9915e2a8c0148f95b78cda9ee9b40a0abd794ca
Wanessa Bezerra <wanessaparelhas68@gmail.com>
2021-04-02 15:45:54 -0300

    CREATE TABLE doador #14

--- docs/create.sql
+++ docs/create.sql
@@ -1,1 +1,9 @@
-
+CREATE TABLE `doador` (
+  `codigo` int(4) NOT NULL,  
+  `nome` varchar(100) NOT NULL,
+  `sexo` char(1) DEFAULT NULL,
+  `tipo_de_sangue` char(3) NOT NULL,
+  `data_de_nascimento` varchar(30) NOT NULL,
+  PRIMARY KEY (`codigo`),
+  foreign key (cod_endereco) references doador(codigo) on delete set null
+);
