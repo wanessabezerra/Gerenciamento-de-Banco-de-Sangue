13c3c10e6fb878c1738ec331a4e6843be91d94ed
Wanessa Bezerra <wanessaparelhas68@gmail.com>
2021-04-02 16:46:23 -0300

    CREATE TABLE endereco #14

--- docs/create.sql
+++ docs/create.sql
@@ -1,9 +1,22 @@
 CREATE TABLE `doador` (
   `codigo` int(4) NOT NULL,  
   `nome` varchar(100) NOT NULL,
-  `sexo` char(1) DEFAULT NULL,
+  `sexo` char(1) NOT NULL,
   `tipo_de_sangue` char(3) NOT NULL,
   `data_de_nascimento` varchar(30) NOT NULL,
   PRIMARY KEY (`codigo`),
   foreign key (cod_endereco) references doador(codigo) on delete set null
 );
+
+CREATE TABLE `endereco` (
+  `codigo` int(4) NOT NULL,  
+  `rua` varchar(50) NOT NULL,
+  `numero` int(4) NOT NULL,
+  `bairro` varchar(50) NOT NULL,
+  `cidade` varchar(50) NOT NULL,
+  `estado` varchar(50) NOT NULL,
+  `cep` int(10) NOT NULL,
+  PRIMARY KEY (`codigo`),
+);
+
+
