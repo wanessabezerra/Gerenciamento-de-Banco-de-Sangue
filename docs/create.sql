4248c644924772d3477c6f9413e9ba80939e122b
Wanessa Bezerra <wanessaparelhas68@gmail.com>
2021-04-03 14:49:44 -0300

    CREATE TABLE telefone e atualização de outras CREATE TABLE #14

--- docs/create.sql
+++ docs/create.sql
@@ -1,46 +1,50 @@
 CREATE TABLE `doador` (
-  `codigo` int(4) NOT NULL,  
-  `nome` varchar(100) NOT NULL,
-  `sexo` char(1) NOT NULL,
-  `tipo_de_sangue` char(3) NOT NULL,
-  `data_de_nascimento` varchar(30) NOT NULL,
+  `codigo` INT(4) PRIMARY KEY,  
+  `nome` VARCHAR(100) NOT NULL,
+  `sexo` CHAR(1) NOT NULL,
+  `tipo_de_sangue` CHAR(3) NOT NULL,
+  `data_de_nascimento` VARCHAR(30) NOT NULL,
   PRIMARY KEY (`codigo`),
-  foreign key (cod_endereco) references doador(codigo) on delete set null
+  foreign key (cod_endereco) references endereco(codigo) on delete set null
+);
+
+CREATE TABLE `telefone` (
+  foreign key (cod_doador) references doador(codigo) on delete set null
+  `numero_fone` CHAR(14) NOT NULL,
 );
 
 CREATE TABLE `endereco` (
-  `codigo` int(4) NOT NULL,  
-  `rua` varchar(50) NOT NULL,
-  `numero` int(4) NOT NULL,
-  `bairro` varchar(50) NOT NULL,
-  `cidade` varchar(50) NOT NULL,
-  `estado` varchar(50) NOT NULL,
+  `codigo` INT(4) PRIMARY KEY,  
+  `rua` VARCHAR(50) NOT NULL,
+  `numero` INT(4) NOT NULL,
+  `bairro` VARCHAR(50) NOT NULL,
+  `cidade` VARCHAR(50) NOT NULL,
+  `estado` VARCHAR(50) NOT NULL,
   `cep` int(10) NOT NULL,
-  PRIMARY KEY (`codigo`),
 );
 
 CREATE TABLE `funcionario` (
-  `codigo` int(4) NOT NULL,  
-  `nome` char(100) NOT NULL,
-  `email` char(256) NOT NULL,
-  `username` char(100) NOT NULL,
+  `codigo` INT(4) NOT NULL,  
+  `nome` CHAR(100) NOT NULL,
+  `email` CHAR(256) NOT NULL,
+  `username` CHAR(100) NOT NULL,
   PRIMARY KEY (`matricula`),
   UNIQUE KEY `username` (`username`)
   foreign key (cod_endereco) references funcionario(codigo) on delete set null
 );
 
 CREATE TABLE `banco_de_sangue` (
-  `codigo` int(4) NOT NULL,  
-  `nome` varchar(50) NOT NULL,
-  `capacidade` int(4) NOT NULL,
+  `codigo` INT(4) NOT NULL,  
+  `nome` VARCHAR(50) NOT NULL,
+  `capacidade` INT(4) NOT NULL,
   PRIMARY KEY (`codigo`),
 );
 
 CREATE TABLE `bolsa_de_sangue` (
-  `codigo` int(4) NOT NULL,  
-  `identificacao_da_doacao` int(4) NOT NULL,
+  `codigo` INT(4) NOT NULL,  
+  `identificacao_da_doacao` INT(4) NOT NULL,
   `data_hora_coleta` DATATIME NOT NULL,
-  `tipo_de_doacao` char(20) NOT NULL,
+  `tipo_de_doacao` CHAR(20) NOT NULL,
   `data_hora_validade` DATATIME NOT NULL,
   PRIMARY KEY (`codigo`),
   foreign key (cod_tipo_sanguineo) references tipo_sanguineo(codigo) on delete set null,
@@ -48,22 +52,22 @@ CREATE TABLE `bolsa_de_sangue` (
 );
 
 CREATE TABLE `tipo_sanguineo` (
-  `codigo` int(4) NOT NULL,  
-  `nome` varchar(50) NOT NULL,
+  `codigo` INT(4) NOT NULL,  
+  `nome` VARCHAR(50) NOT NULL,
   PRIMARY KEY (`codigo`),
 );
 
 CREATE TABLE `tipo_de_hemocomponente` (
-  `codigo` int(4) NOT NULL,  
-  `nome` varchar(100) NOT NULL,
-  `descricao` varchar(256) NOT NULL,
+  `codigo` INT(4) NOT NULL,  
+  `nome` VARCHAR(100) NOT NULL,
+  `descricao` VARCHAR(256) NOT NULL,
   PRIMARY KEY (`codigo`),
 );
 
 CREATE TABLE `saida` (
-  `codigo` int(4) NOT NULL,  
-  `nome_do_hospital` varchar(100) NOT NULL,
-  `nome_do_paciente` varchar(100) NOT NULL,
+  `codigo` INT(4) NOT NULL,  
+  `nome_do_hospital` VARCHAR(100) NOT NULL,
+  `nome_do_paciente` VARCHAR(100) NOT NULL,
   `data_hora_de_saida` DATATIME NOT NULL,
   PRIMARY KEY (`codigo`),
 );
