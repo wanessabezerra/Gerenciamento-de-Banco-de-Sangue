04188443586e0cef245973b5158424adf57a8fad
Wanessa Bezerra <wanessaparelhas68@gmail.com>
2021-04-03 15:10:20 -0300

    Atualizando as FK #14

--- docs/create.sql
+++ docs/create.sql
@@ -4,13 +4,14 @@ CREATE TABLE `doador` (
   `sexo` CHAR(1) NOT NULL,
   `tipo_de_sangue` CHAR(3) NOT NULL,
   `data_de_nascimento` VARCHAR(30) NOT NULL,
-  PRIMARY KEY (`codigo`),
-  foreign key (cod_endereco) references endereco(codigo) on delete set null
+  `cod_endereco` INT NOT NULL,
+  CONSTRAINT `fk_endereco` FOREIGN KEY (`cod_endereco`) REFERENCES endereco(codigo) on delete set null
 );
 
 CREATE TABLE `telefone` (
-  foreign key (cod_doador) references doador(codigo) on delete set null
+  `cod_doador` INT PRIMARY KEY,
   `numero_fone` CHAR(14) NOT NULL,
+  CONSTRAINT `fk_doador` FOREIGN KEY (`cod_doador`) REFERENCES doador(codigo) on delete set null
 );
 
 CREATE TABLE `endereco` (
@@ -24,24 +25,25 @@ CREATE TABLE `endereco` (
 );
 
 CREATE TABLE `funcionario` (
-  `codigo` INT(4) NOT NULL,  
+  `codigo` INT(4) PRIMARY KEY,  
   `nome` CHAR(100) NOT NULL,
   `email` CHAR(256) NOT NULL,
   `username` CHAR(100) NOT NULL,
   PRIMARY KEY (`matricula`),
   UNIQUE KEY `username` (`username`)
+  CONSTRAINT `fk_doador` FOREIGN KEY (`cod_doador`) REFERENCES doador(codigo) on delete set null
   foreign key (cod_endereco) references funcionario(codigo) on delete set null
 );
 
 CREATE TABLE `banco_de_sangue` (
-  `codigo` INT(4) NOT NULL,  
+  `codigo` INT(4) PRIMARY KEY,  
   `nome` VARCHAR(50) NOT NULL,
   `capacidade` INT(4) NOT NULL,
   PRIMARY KEY (`codigo`),
 );
 
 CREATE TABLE `bolsa_de_sangue` (
-  `codigo` INT(4) NOT NULL,  
+  `codigo` INT(4) PRIMARY KEY,  
   `identificacao_da_doacao` INT(4) NOT NULL,
   `data_hora_coleta` DATATIME NOT NULL,
   `tipo_de_doacao` CHAR(20) NOT NULL,
@@ -52,20 +54,20 @@ CREATE TABLE `bolsa_de_sangue` (
 );
 
 CREATE TABLE `tipo_sanguineo` (
-  `codigo` INT(4) NOT NULL,  
+  `codigo` INT(4) PRIMARY KEY,  
   `nome` VARCHAR(50) NOT NULL,
   PRIMARY KEY (`codigo`),
 );
 
 CREATE TABLE `tipo_de_hemocomponente` (
-  `codigo` INT(4) NOT NULL,  
+  `codigo` INT(4) PRIMARY KEY,  
   `nome` VARCHAR(100) NOT NULL,
   `descricao` VARCHAR(256) NOT NULL,
   PRIMARY KEY (`codigo`),
 );
 
 CREATE TABLE `saida` (
-  `codigo` INT(4) NOT NULL,  
+  `codigo` INT(4) PRIMARY KEY,  
   `nome_do_hospital` VARCHAR(100) NOT NULL,
   `nome_do_paciente` VARCHAR(100) NOT NULL,
   `data_hora_de_saida` DATATIME NOT NULL,
