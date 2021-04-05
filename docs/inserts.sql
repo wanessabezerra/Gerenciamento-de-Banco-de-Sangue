b65b806c0441dc1b36c8411d07d5f44b2e77d4c0
renatak12 <54039830+renatak12@users.noreply.github.com>
2021-04-05 02:58:07 -0300

    Add Inserts tabelas telefone e funcionário #15
    #15

--- docs/inserts.sql
+++ docs/inserts.sql
@@ -15,3 +15,21 @@ INSERT INTO doador(codigo, nome, sexo, tipo_de_sangue, data_de_nascimento, cod_e
 		(0215, "João", "M", "B-", "1994-12-10", 0115),
 		(0216, "Ana", "F", "AB+", "1989-11-27", 0116)
 ;
+
+INSERT INTO telefone(cod_doador, numero_fone)
+	VALUES
+		(0212, "(84)9979-3943"),
+    		(0213, "(84)9834-3753"),
+    		(0214, "(84)9987-8863"),
+    		(0215, "(84)9836-9876"),
+    		(0216, "(84)9820-2469")
+;
+
+INSERT INTO funcionario (codigo, nome, email, username, cod_endereco)
+	VALUES
+   		(0312, "Marcos", "marcos10@hotmail.com", "marcos_10", 0116),
+    		(0313, "José", "jose1234@gmail.com", "jose_123", 0114),
+    		(0314, "Mariana", "mariana12@outlook.com", "mariana_12", 0113),
+    		(0315, "Wanessa","wanessaparelhas@gmail.com", "wanessa_parelhas", 0112),
+    		(0316, "Lucas", "lucas321@gmail.com", "lucas_321", 0115);
+;
