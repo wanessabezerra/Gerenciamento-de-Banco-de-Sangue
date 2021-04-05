d63c0ab26ef3840be13319fca5bf9f7b93ee3cc2
renatak12 <54039830+renatak12@users.noreply.github.com>
2021-04-05 02:46:23 -0300

    Update inserts.sql
    Add inserts das tabelas de endereço e doador #15

--- docs/inserts.sql
+++ docs/inserts.sql
@@ -1,1 +1,17 @@
+INSERT INTO endereco(codigo, rua, numero, bairro, cidade, estado, cep)
+	VALUES
+		(0112, "Rua Pedro Velho", 591, "centro", "Caico", "RN", 59300000),
+		(0113, "Av Serido", 380, "centro", "Caico", "RN", 59300000),
+		(0114, "Rua Joel Damasceno", 196, "centro", "Caico", "RN", 59300000),
+		(0115, "Av Josias Miguel", 623, "Nova caicó", "Caico", "RN", 59300000),
+		(0116, "Rua Renato Dantas", 243, "centro", "Caico", "RN", 59300000)
+;
 
+INSERT INTO doador(codigo, nome, sexo, tipo_de_sangue, data_de_nascimento, cod_endereco)
+  VALUES
+    (0212, "Maria", "F", "O+", "1990-05-12", 0112),
+    (0213, "Julia", "F", "O-", "1995-07-10", 0113),
+    (0214, "Matheus", "M", "A+", "1990-04-08", 0114),
+    (0215, "João", "M", "B-", "1994-12-10", 0115),
+    (0216, "Ana", "F", "AB+", "1989-11-27", 0116)
+;
